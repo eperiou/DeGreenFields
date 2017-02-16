bdf0b7a78d156482c3dcf1bc15a1b5924c0307cd
defields923 <defields923@gmail.com>
2017-02-16 13:45:13 -0600

    ange for heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.resolve(__dirname, './build/index.html'));
 });
 
