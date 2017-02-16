c4d001e58fac58dda1e19d3ff9095536c72f0980
defields923 <defields923@gmail.com>
2017-02-16 13:33:29 -0600

    added procfile and build

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+let path = require('path');
 const express = require('express');
 const app = express();
 let PORT = process.env.PORT || 3000;
 
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World');
-})
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, './public/index.html'));
+});
 
 
 app.listen(PORT, function() {
