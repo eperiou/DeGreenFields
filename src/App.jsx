c1374a43968a69ad0fbbde69f56f3a8f08694ddb
Eric Periou <eperiou@yahoo.com>
2017-02-19 11:57:20 -0600

    Moved signup to own file

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,24 @@ import './App.css';
 class App extends Component {
 constructor () {
   super()
-  this.userLogin = this.userLogin.bind(this);
+  this.state = null;
 }
 // userLogin () {
 //   alert('hello')
 // }
+  this.userLogin() {
+    console.log('login');
+  }
 
 render() {
-    return (
-      <div className="container">
-        <h1 className="welcome text-center">Welcome to Whats Happenin'</h1>
-            <div className="card card-container">
-            <h2 className="login_title text-center">Login</h2>
-
-            <form  className="form-signin">
-                <span id="reauth-email" className="reauth-email"></span>
-                <p className="input_title">Email</p>
-                    <input type="text" id="inputEmail" className="login_box"
-                      placeholder="user01@placename.com"></input>
-                    <p className="input_title">Password</p>
-                    <input type="password" id="inputPassword" className="login_box" placeholder="******" required></input>
-                    <div id="remember" className="checkbox">
-                        <label>
-
-                        </label>
-                    </div>
-                    <button onSubmit={this.userLogin()}
-                       className="btn btn-lg btn-primary" type="submit">Login</button>
-              </form>
-          </div>
+  return (
+      <div>
+        <Signin />
       </div>
-    );
-  }
 
-  userLogin (){
-      console.log('login');
-  }
+  );
+}
+
 
 }
 
