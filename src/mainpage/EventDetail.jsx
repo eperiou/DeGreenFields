e9cd692e5d2db9f5f94cd941ebd8fd1a07b4b875
Eric Periou <eperiou@yahoo.com>
2017-02-19 17:55:37 -0600

    Adds event list, event detailed boxes, passes down event list to event componenet, added in bootstrap

--- src/mainpage/EventDetail.jsx
+++ src/mainpage/EventDetail.jsx
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class EventDetail extends Component {
-  constructor(props) {
-    super();
-
-  }
-  render() {
-    return (
-      <h1>MAP!</h1>
-    )
-  }
+const EventDetail = (props) => {
+  return (
+    <article className="eventdetail">
+      <img src="http://www.ufunk.net/wp-content/uploads/2013/03/thumbs-and-ammo-9.jpg" className="col-md-2"></img>
+      <section className="eventdescription">
+        <div class="col-md-3">
+          <h3>Eventheader</h3>
+            <div>username: String,</div>
+            <div>eventTime: Date,</div>
+            <a>location: String, link to map</a>
+            <p> full event description</p>
+        </div>
+      </section>
+    </article>
+  );
 }
-export default App;
+//
+//
+
+// createdAt: Date, // In the format of "2/16/2017, 8:28:03 PM"
+// tags: Array,
+// businessName: String,
+// picLink: String, // Link to hosted picture
+// busLink: String, // url to business' homepage
+// description: String,
+// });
+export default EventDetail;
