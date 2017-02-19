3f7dc83e538054bb441656fb4445043f8714033e
Eric Periou <eperiou@yahoo.com>
2017-02-19 17:55:37 -0600

    Adds event list, event detailed boxes, passes down event list to event componenet, added in bootstrap

--- src/mainpage/events_mappage.jsx
+++ src/mainpage/events_mappage.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import EventDetail from './EventDetail';
-import Nav from './nav.jsx';
+import EventDetail from './eventDetail.jsx';
+import EventList from './eventList.jsx';
+import Map from './Map.jsx';
 
 class Map_Page extends Component {
   constructor() {
     super();
     this.state = {
-      eventList: [],
+      eventList: ['event1',   'event2', 'event3', 'event4', 'event5'],
       map: null,
       detailsBox: null,
     };
@@ -20,22 +21,44 @@ class Map_Page extends Component {
   render() {
     return (
       <body>
-        //nav bar will have the search and filter methods.
-        <Nav />
-        <main id="map">
-          <section>
-            //map will hold google maps
-              <Map />
-          </section>
-          <article>
-            //plan for object of event to be passed through
-            <EventDetail Event={this.detailsBox}/>
-          </article>
-        </main>
-        <sidebar>
-          //want the eventlis items to update the Detailsbox on click
-          <EventList setDetailsBox={this.setDetailsBox}/>
-        </sidebar>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+         <div class="container">
+           <div class="navbar-header">
+             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+               <span class="sr-only">Toggle navigation</span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+             </button>
+             <a class="navbar-brand" href="#">Project name</a>
+           </div>
+           <div id="navbar" class="collapse navbar-collapse">
+             <ul class="nav navbar-nav">
+               <li class="active"><a href="#">Home</a></li>
+               <li><a href="#about">About</a></li>
+               <li><a href="#contact">Contact</a></li>
+             </ul>
+           </div>
+         </div>
+       </nav>
+
+       <div class="container">
+           <main id="map">
+             <section>
+               //map will hold google maps
+                 <Map />
+             </section>
+             <section id="EventDetail">
+               //plan for object of event to be passed through
+               <EventDetail Event={this.detailsBox}/>
+             </section>
+           </main>
+           <sidebar>
+             //want the eventlis items to update the Detailsbox on click
+             <EventList eventList={this.state.eventList} setDetailsBox={this.setDetailsBox}/>
+           </sidebar>
+         </div>
+              //nav bar will have the search and filter methods.
       </body>
     )
   }
