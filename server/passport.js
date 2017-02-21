5c4c1cbe46da704fc017b0142c4e0ed15b44f523
defields923 <defields923@gmail.com>
2017-02-20 18:57:46 -0600

    (feature) implement authorization through passport

--- server/passport.js
+++ server/passport.js
@@ -6,6 +6,18 @@ const configAuth = require('./auth.js');
 const RegularUser = require('./../models/userSchema.js');
 
 module.exports = (passport) => {
+  // serialize and deserialize user when signing in and out
+  passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+  });
+
+  passport.deserializeUser((id, cb) => {
+    RegularUser.findById(id, (err, user) => {
+      if (err) { return cb(err); }
+      cb(null, user);
+    });
+  });
+
   // Local login
 
   passport.use(new LocalStrategy(
@@ -20,35 +32,55 @@ module.exports = (passport) => {
         }
         return done(null, user);
       });
-    },
+    }
   ));
   passport.use(new GitHubStrategy({
     clientID: configAuth.githubAuth.clientID,
     clientSecret: configAuth.githubAuth.clientSecret,
     callbackURL: configAuth.githubAuth.callbackURL,
-  }, (token, refreshToken, profile, done) => {
+  }, (req, token, refreshToken, profile, done) => {
     process.nextTick(() => {
-      RegularUser.findOne({ 'github.id': profile.id }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
-        if (user) {
-          return done(null, user);
-        }
-        const newUser = new RegularUser();
-
-        newUser.github.id = profile.id;
-        newUser.github.username = profile.username;
-        newUser.github.displayName = profile.displayName;
-        newUser.github.publicRepos = profile._json.public_repos;
-
-        return newUser.save((error) => {
-          if (error) {
-            throw error;
+      if (!req.user) {
+        RegularUser.findOne({ 'github.id': profile.id }, (err, user) => {
+          if (err) {
+            return done(err);
           }
-          return done(null, newUser);
+          if (user) {
+            return done(null, user);
+          }
+          const newUser = new RegularUser();
+
+          // TODO: Refactor dry
+          newUser.github.id = profile.id;
+          newUser.github.username = profile.username;
+          newUser.github.displayName = profile.displayName;
+          newUser.github.publicRepos = profile._json.public_repos;
+
+          // TODO: Change to promise
+          return newUser.save((error) => {
+            if (error) {
+              throw error;
+            }
+            return done(null, newUser);
+          });
         });
-      });
+      } else {
+        // user already exists and is logged in, we have to link accounts
+        const user = req.user; // pull the user out of the session
+        // update the current users facebook credentials
+        user.google.id = profile.id;
+        user.google.token = token;
+        user.google.name = profile;
+        user.google.email = profile.emails[0].value;
+
+        // save the user
+        user.save((err) => {
+          if (err) {
+            throw err;
+          }
+          return done(null, user);
+        });
+      }
     });
   }));
 
@@ -56,35 +88,53 @@ module.exports = (passport) => {
     clientID: configAuth.googleAuth.clientID,
     clientSecret: configAuth.googleAuth.clientSecret,
     callbackURL: configAuth.googleAuth.callbackURL,
-  }, (token, refreshToken, profile, done) => {
+  }, (req, token, refreshToken, profile, done) => {
         // make the code asynchronous
         // User.findOne won't fire until we have all our data back from Google
     process.nextTick(() => {
+      if (!req.user) {
             // try to find the user based on their google id
-      RegularUser.findOne({ 'google.id': profile.id }, (err, user) => {
-        if (err) {
-          return done(err);
-        }
-        if (user) {
-            // if a user is found, log them in
-          return done(null, user);
-        }
-          // if the user isnt in our database, create a new user
-        const newUser = new RegularUser();
-          // set all of the relevant information
-        newUser.google.id = profile.id;
-        newUser.google.token = token;
-        newUser.google.name = profile.displayName;
-        newUser.google.email = profile.emails[0].value; // pull the first email
+        RegularUser.findOne({ 'google.id': profile.id }, (err, user) => {
+          if (err) {
+            return done(err);
+          }
+          if (user) {
+              // if a user is found, log them in
+            return done(null, user);
+          }
+            // if the user isnt in our database, create a new user
+          const newUser = new RegularUser();
+            // set all of the relevant information
+          newUser.google.id = profile.id;
+          newUser.google.token = token;
+          newUser.google.name = profile.displayName;
+          newUser.google.email = profile.emails[0].value; // pull the first email
+
+          // save the user
+          return newUser.save((error) => {
+            if (error) {
+              throw error;
+            }
+            return done(null, newUser);
+          });
+        });
+      } else {
+        // user already exists and is logged in, we have to link accounts
+        const user = req.user; // pull the user out of the session
+        // update the current users facebook credentials
+        user.google.id = profile.id;
+        user.google.token = token;
+        user.google.name = profile;
+        user.google.email = profile.emails[0].value;
 
         // save the user
-        return newUser.save((error) => {
-          if (error) {
-            throw error;
+        user.save((err) => {
+          if (err) {
+            throw err;
           }
-          return done(null, newUser);
+          return done(null, user);
         });
-      });
+      }
     });
   }));
 
@@ -95,37 +145,55 @@ module.exports = (passport) => {
     callbackURL: configAuth.facebookAuth.callbackURL,
   },
   // facebook will send back the token and profile
-  (token, refreshToken, profile, done) => {
+  (req, token, refreshToken, profile, done) => {
       // asynchronous
     process.nextTick(() => {
+      if (!req.user) {
       // find the user in the database based on their facebook id
-      RegularUser.findOne({ 'facebook.id': profile.id }, (err, user) => {
-        // if there is an error, stop everything and return that
-        // ie an error connecting to the database
-        if (err) {
-          return done(err);
-        }
-        // if the user is found, then log them in
-        if (user) {
-          return done(null, user); // user found, return that user
-        }
-        // if there is no user found with that facebook id, create them
-        const newUser = new RegularUser();
-
-        // set all of the facebook information in our user model
-        newUser.facebook.id = profile.id; // set the users facebook id
-        newUser.facebook.token = token; // we will save the token that facebook provides to the user
-        newUser.facebook.name = `${profile.name.givenName} ${profile.name.familyName}`; // look at the passport user profile to see how names are returned
-        newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
-        // save our user to the database
-        return newUser.save((error) => {
-          if (error) {
-            throw error;
+        RegularUser.findOne({ 'facebook.id': profile.id }, (err, user) => {
+          // if there is an error, stop everything and return that
+          // ie an error connecting to the database
+          if (err) {
+            return done(err);
           }
-          // if successful, return the new user
-          return done(null, newUser);
+          // if the user is found, then log them in
+          if (user) {
+            return done(null, user); // user found, return that user
+          }
+          // if there is no user found with that facebook id, create them
+          const newUser = new RegularUser();
+
+          // set all of the facebook information in our user model
+          newUser.facebook.id = profile.id; // set the users facebook id
+          newUser.facebook.token = token; // we will save the token that facebook provides to the user
+          newUser.facebook.name = `${profile.name.givenName} ${profile.name.familyName}`; // look at the passport user profile to see how names are returned
+          newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+          // save our user to the database
+          return newUser.save((error) => {
+            if (error) {
+              throw error;
+            }
+            // if successful, return the new user
+            return done(null, newUser);
+          });
         });
-      });
+      } else {
+        // user already exists and is logged in, we have to link accounts
+        const user = req.user; // pull the user out of the session
+        // update the current users facebook credentials
+        user.facebook.id = profile.id;
+        user.facebook.token = token;
+        user.facebook.name = `${profile.name.givenName} ${profile.name.familyName}`;
+        user.facebook.email = profile.emails[0].value;
+
+        // save the user
+        user.save((err) => {
+          if (err) {
+            throw err;
+          }
+          return done(null, user);
+        });
+      }
     });
   }));
 };
